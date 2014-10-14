14a90b26d436e53c5106210cbd787118c4d8b9ac
André Olsson <ao222pq@student.lnu.se>
2014-10-14 19:10:58 +0200

    starting on lab 2-2

---
+++ Labb 2_2 väckarklocka/Labb 2_2 väckarklocka/AlarmClock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_2_2_väckarklocka
+{
+    class AlarmClock
+    {
+        private int _hour;
+        private int _minute;
+        private int _alarmHour;
+        private int _alarmMinute;
+
+        public AlarmClock()
+        {
+            new AlarmClock(0, 0);
+        }
+
+        public AlarmClock(int hour, int minute)
+        {
+            new AlarmClock(hour, minute, 0, 0);
+        }
+
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+
+        }
+
+
+    }
+}
---
+++ Labb 2_2 väckarklocka/Labb 2_2 väckarklocka/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_2_2_väckarklocka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
